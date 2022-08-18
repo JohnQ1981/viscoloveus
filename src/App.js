f69e30605823395fd1fed9e05db1b728bdf1c8fb
JohnQ1981 <ikram1981@gmail.com>
2022-08-17 21:29:30 -0500

    added sign in

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.components";
-const Shop = () => {
-  return (
-    <div>
-      <h2>I am the Shop</h2>
-    </div>
-  );
-};
+import Navigation from "./routes/navigation/navigation";
+import Shop from "./routes/shop/shop";
+import SignIn from "./routes/sing-in/sing-in.component";
 
-const Navigation = () => {
-  return (
-    <div>
-      <h2>I am the NavigationBar</h2>
-      <Outlet />
-    </div>
-  );
-};
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="signIn" element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/shop/shop.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+const Shop = () => {
+    return (
+      <div>
+        <h2>I am the Shop</h2>
+      <Outlet />
+      </div>
+    );
+  };
+  export default Shop;
